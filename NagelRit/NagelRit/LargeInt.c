9ebeb5ae11b782b8df6df6039fe3ff66e3afe0ca
Jacobus <jacobus.conradi@gmx.de>
2016-11-03 22:01:57 +0100

    added comments for all uncommented functions

--- NagelRit/NagelRit/LargeInt.c
+++ NagelRit/NagelRit/LargeInt.c
@@ -188,7 +188,12 @@ void destructor(struct LargeInt * li) {
 	free(li->LInt);
 	free(li);
 }
-
+/* shifts the given LargeInt. The function returns a new LargeInt
+   object.
+   It first shifts by an amount of bytes to be faster, and then iterates
+   through the array and shifts every char-cell by an amount smaller
+   or equal to 8
+*/
 struct LargeInt * bitshiftup(struct LargeInt * li, int amount) {
 	if (amount <= 0) {
 		assert(0);
@@ -224,7 +229,15 @@ struct LargeInt * bitshiftup(struct LargeInt * li, int amount) {
 	return result;
 }
 
+/* multiplies two LargeInts A and B and returns a new one. It adds 
+   bitshifted versions of A into the result.
+             0x0A * 0x0A = 0x64
+   As bits: 1010 * 1010 = 01100100 =
+                             10100 +
+			   1010000
 
+   Runtime: O(log(A + B));
+*/
 struct LargeInt * mult(struct LargeInt * lia, struct LargeInt * lib) {
 	struct LargeInt * result = malloc(sizeof(struct LargeInt));
 	result->LInt = calloc(lia->size + lib->size, 1);
@@ -281,7 +294,11 @@ struct LargeInt * mult(struct LargeInt * lia, struct LargeInt * lib) {
 	return result;
 }
 
+/* returns a LargeInt that approximates 1/10 bitshifted by length.
+   This LargeInt is used as a constant in divideByThen
+*/
 struct LargeInt * oneTenth(int length) {
+	if(length == 0)assert(0);
 	struct LargeInt * oneTenth = (struct LargeInt *)malloc(sizeof(struct LargeInt));
 	oneTenth->LInt = malloc(length);
 	oneTenth->size = length;
@@ -293,7 +310,26 @@ struct LargeInt * oneTenth(int length) {
 	return oneTenth;
 }
 
-struct LargeInt * divideByTen(struct LargeInt * li, struct LargeInt * oT) {
+/* recieves a LargeInt and a constant oT. The LargeInt is any LargeInt
+   which one wants to divide by a constant > 1. oT is supposed to be an approximation
+   of the inverse of the desired value byteshifted by oT->size to not be rounded by 0
+   
+   For example if one wants to divide a value with 6 hexadecimal digits by 10, one
+   would have to call 
+
+
+   LargeInt* oT = oneTenth(3); ** 3 because oneTenth returns byteshifted, meaning 
+				 each char-cell contains 2 digits **
+   LargeInt * dividedvalue = divideByMultAndBitshift(value,oT);
+   destructor(oT);
+
+
+   returns a byteshifted multiplication of the given values.
+   if oT is properly chosen, this can be used to divide
+  
+   Runtime(log(n)²)
+*/
+struct LargeInt * divideByMultAndBitshift(struct LargeInt * li, struct LargeInt * oT) {
 	struct LargeInt * ph = mult(li, oT);
 	struct LargeInt * result = (struct LargeInt *)malloc(sizeof(struct LargeInt));
 	result->LInt = malloc(ph->size - oT->size);
@@ -308,7 +344,8 @@ struct LargeInt * divideByTen(struct LargeInt * li, struct LargeInt * oT) {
 	destructor(ph);
 	return result;
 }
-
+/* removes the useless back of a LargeInt to remove basically useless memory
+*/
 void cutEnd(struct LargeInt * li) {
 	for (int i = li->size - 1; i >= 0; i--) {
 		if (li->LInt[i] != 0) {
@@ -318,14 +355,21 @@ void cutEnd(struct LargeInt * li) {
 		}
 	}
 }
-/* TODO: recieves a LargeInt and returns a String, depicting the decimal value of the array
-   {&(0x30),1} -> "48"
+/* each digit in a hexadecimal number apart from the lowest manipulates the lowest
+   digit of the decimal version by a factor of 6. This function takes a character, 
+   and checks if it is a halfbyte (only lowest 4 bits are set), and then returns, how
+   much the given char in hexadecimal manipulates the decimal lowest digit
 */
 short intFromHalfByte(char a) {
+	if(char > 0x0f)assert(0);
 	return ((short)a) * 6 % 10;
 }
 
-
+/* recieves a LargeInt to return the lowest digit of hits decimal version.
+   each digit in a hexadenimal number apart from the lowest manipulates the lowest
+   digit of the decimal version by a factor of 6. This function sums up all the manipulation
+   of all the hexadecimal digits with the use of intFromHalfByte(char), returning the
+   lowest digit in the decimal version of the hexadecimal LargeInt*/
 char lowestdigitinDec(struct LargeInt * lint) {
 	long long sum = 0;
 	for (int i = 1; i < lint->size; ++i) {
@@ -335,7 +379,13 @@ char lowestdigitinDec(struct LargeInt * lint) {
 	return (sum % 10) + '0';
 }
 
-
+/* recieves a LargeInt and calculates the decimal version into a string which will be returned.
+   It repeatedly divides the LargeInt by 10, and caclulates the lowest decimal digit with
+   divideByMultAndBitshit(LI*,LI*) and lowestdigitinDec(LI*)
+   
+   Runtime: O(log(n)³)
+   sadly rather slow for a function that is potentionally called alot.
+*/
 char * LargeIntToString_Dec(struct LargeInt * lint)
 {
 	if (lint->size == 0) {
@@ -344,13 +394,13 @@ char * LargeIntToString_Dec(struct LargeInt * lint)
 	char * result = (char *)malloc( (int) lint->size * 4);
 	result[0] = lowestdigitinDec(lint);
 	struct LargeInt* oT = oneTenth(lint->size+1);
-	struct LargeInt* iterator = divideByTen(lint,oT);
+	struct LargeInt* iterator = divideByMultAndBitshift(lint,oT);
 	struct LargeInt* oldLI;
 	int i;
 	for (i = 1;iterator->size > 1 || (iterator->size == 1 && iterator->LInt[0] != 0x0); ++i) {
 		oldLI = iterator;
 		result[i] = lowestdigitinDec(iterator);
-		iterator = divideByTen(iterator,oT);
+		iterator = divideByMultAndBitshift(iterator,oT);
 		destructor(oldLI);
 	}
 	--i;
--- NagelRit/NagelRit/LargeInt.h
+++ NagelRit/NagelRit/LargeInt.h
@@ -9,7 +9,7 @@ struct LargeInt * NEW_LargeInt_from_str(const char * string, unsigned int size);
 struct LargeInt * add(struct LargeInt * lia, struct LargeInt * lib);
 char * LargeIntToString_Hex(struct LargeInt * lint);
 char * LargeIntToString_Dec(struct LargeInt * lint);
-struct LargeInt * divideByTen(struct LargeInt * li, struct LargeInt* oT);
+struct LargeInt * divideByMultAndBitshift(struct LargeInt * li, struct LargeInt* oT);
 struct LargeInt * bitshiftup(struct LargeInt * li, int amount);
 struct LargeInt * mult(struct LargeInt * lia, struct LargeInt * lib);
 void destructor(struct LargeInt * li);
